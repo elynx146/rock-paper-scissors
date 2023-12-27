59dee1868077ce46844f0d4574b8b03c3d60fae4
elynx146 <147456824+elynx146@users.noreply.github.com>
2023-12-26 19:44:06 -0500

    "rock paper scissors game function"

--- javascript.js
+++ javascript.js
@@ -7,49 +7,115 @@
 
 //return score +1 for winner
 
-let playerscore
-let compscore
-let tie
-tie = 1
+let playerscore;
+let compscore;
+let gamenum;
+gamenum = 5;
+playerscore = 0
+compscore = 0
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase();
     if (playerSelection === "rock"){
         if(computerSelection === "scissors"){
-            return (playerscore + 1)
+            return (playerscore + 1);
+        }
         if(computerSelection === "paper"){
-            return (compscore +1)
+            //return (compscore +1);
+            return (playerscore - 1);
         }
         if(computerSelection === "rock"){
-            return tie
-        }
+            return 0;
         }
     }
     if (playerSelection === "paper"){
         if(computerSelection === "rock"){
-            return (playerscore + 1)
+            return (playerscore + 1);
+        }
         if(computerSelection === "scissors"){
-            return (compscore +1)
+            //return (compscore +1);
+            return (playerscore + 1);
         }
         if(computerSelection === "paper"){
-            return tie
-        }
+            return 0;
         }
     }
     if (playerSelection === "scissors"){
         if(computerSelection === "paper"){
-            return (playerscore + 1)
+            return (playerscore + 1);
+        }
         if(computerSelection === "rock"){
-            return (compscore +1)
+            //return (compscore +1);
+            return (playerscore + 1);
         }
         if(computerSelection === "scissors"){
-            return tie
+            return 0;
         }
     }
-  }
+    console.log("error")
+    return(0)
 }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  
+
+let compChoice
+
+function getComputerChoice() {
+    //pick random number between 1 & 3
+    compChoice = Math.floor(Math.random() * 3) + 1;
+    console.log(compChoice);
+    let compOut;
+    if(compChoice === 1){
+        compOut = "rock";
+        return compOut;
+    }
+    if(compChoice === 2){
+        compOut = "paper";
+        return compOut;
+    }
+    if(compChoice === 3){
+        compOut = "scissors";
+        return compOut;
+    }
+    else{
+        console.log("ERROR");
+    }
+}
+
+//const playerSelection = prompt("rock, paper, scissors?:");
+//const computerSelection = getComputerChoice();
+//console.log(computerSelection)
+//console.log(playRound(playerSelection, computerSelection));
+let finalscore;
+finalscore = 0
+
+function game(){
+while(gamenum > 0){
+    let playerSelection = prompt("rock, paper, scissors?:");
+    let computerSelection = getComputerChoice();
+    result = playRound(playerSelection,computerSelection);
+    if(result === 0){
+        gamenum = gamenum;
+    }
+    else{
+        gamenum = gamenum - 1;
+    }
+    finalscore = result + finalscore;
+    if(finalscore === 3){
+        gamenum = 0
+    }
+    if(finalscore === -3){
+        gamenum = 0
+    }
+}
+if(finalscore < 3){
+    console.log("lose")
+    console.log(finalscore)
+    return ("The Player Lost");
+}
+else{
+    console.log("win")
+    console.log(finalscore)
+    return ("The Player Won");
+}
+}
+
+game()
